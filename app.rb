1059e64302503f7c8179e6e53139f60c30830824
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-19 03:15:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params
+    @abbv = {
+      name: "Team Name",
+      coach: "Coach",
+      pg: "Point Guard",
+      sg: "Shooting Guard",
+      sf: "Small Forward",
+      c: "Center"
+    }
+    erb :team
+  end
+
 
 end
