cc7f76aba70fb4d35e0ac3c8a57fc4cd5f05cb9e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-19 03:27:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params
+    @abbv = {
+      name: "Team Name",
+      coach: "Coach",
+      pg: "Point Guard",
+      sg: "Shooting Guard",
+      sf: "Small Forward",
+      pf: "Power Forward",
+      c: "Center"
+    }
+    erb :team
+  end
+
 
 end
